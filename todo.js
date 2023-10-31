81791a219d75fde3ec2c4de3f9b01e3ef9e4ace8
adhikari-anil <140706658+adhikari-anil@users.noreply.github.com>
2023-10-31 21:55:50 +0545

    Updated todo.js

--- todo.js
+++ todo.js
@@ -1,4 +1,3 @@
-// const tasks = [];
 let text = document.getElementById("newTask");
 let listContainer = document.querySelector(".todo");
 let tasks = [];
@@ -18,30 +17,27 @@ function myFunction() {
   if (taskText === "") {
     alert("You must put at least one task!");
   } else {
-    const task = `<li><input type="checkbox"> ${taskText} <img src="https://cdn2.vectorstock.com/i/1000x1000/64/71/diskette-black-icon-save-button-vector-31046471.jpg"></li>`;
+    const task = `<li><input type="checkbox"> ${taskText} </li>`;
     listContainer.innerHTML += task;
-    //tasks.push(taskText);
+    tasks.push(taskText); // Push the task text into the tasks array
     text.value = "";
+    save(); // Call save() to update the localStorage
   }
 }
 
 listContainer.addEventListener("click", function (event) {
   const target = event.target;
   const location = target.parentElement;
-  const deleteitem = location.innerText;
-  //console.log(deleteitem);
+
   if (target.type === "checkbox") {
-    target.parentElement.classList.toggle("checked");
-    setInterval(() => {
-      /*
-      //remove(location);
-      const tasklist = target.nextSibling.textContent.trim(); // Get the task text
-      const taskIndex = tasks.indexOf(tasklist);
+    location.classList.toggle("checked");
+    setTimeout(() => {
+      const taskText = location.innerText.trim();
+      const taskIndex = tasks.indexOf(taskText);
       if (taskIndex !== -1) {
         tasks.splice(taskIndex, 1);
+        update(); // Call update() to update the localStorage
       }
-      */
-      update();
       location.remove();
     }, 1000);
   } else if (target.tagName === "IMG") {
@@ -49,40 +45,18 @@ listContainer.addEventListener("click", function (event) {
   }
 });
 
-function save(deleteitem) {
-  tasks.push(deleteitem);
+function save() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 function update() {
-  const tasklist = target.nextSibling.textContent.trim(); // Get the task text
-  const taskIndex = tasks.indexOf(tasklist);
-  if (taskIndex !== -1) {
-    tasks.splice(taskIndex, 1);
-  }
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 function show() {
   listContainer.innerHTML = ""; // Clear the list
   tasks.forEach((taskText) => {
-    const task = `<li><input type="checkbox"> ${taskText} <img src="https://cdn2.vectorstock.com/i/1000x1000/64/71/diskette-black-icon-save-button-vector-31046471.jpg"></li>`;
+    const task = `<li><input type="checkbox"> ${taskText} </li>`;
     listContainer.innerHTML += task;
   });
 }
-
-/*
-let inputs = document.getElementById("newTask");
-let text = document.querySelector(".todo");
-
-function myFunction() {
-    if (inputs.value == "") {
-        alert("Please Enter Task");
-    } else {
-        let newEle = document.createElement("ul");
-        newEle.innerHTML = `${inputs.value}`;
-        text.appendChild(newEle);
-        inputs.value = "";
-    }
-}
-*/
